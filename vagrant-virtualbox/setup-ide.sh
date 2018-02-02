4d69c8396f1980ad3c71c0374860fceb140df48a
Christian Schuhegger <Christian.Schuhegger@gmx.de>
2018-02-02 08:02:40 +0100

    dev-meetup-rust-up/vagrant-virtualbox: set-up workspaces and ides

---
+++ vagrant-virtualbox/setup-ide.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /media/disk2/home/cs/local/install/ideaIU-2017.3.3.tar.gz ./ideaIU-2017.3.3.tar.gz
+cp /media/disk2/home/cs/local/install/CLion-2017.3.2.tar.gz ./CLion-2017.3.2.tar.gz
